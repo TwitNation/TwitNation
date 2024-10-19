c2ec9f8be688191654e6245dd8288184eff6a752
ㅇi쀼z1 <yaewon0411@naver.com>
2024-10-20 00:11:20 +0900

    feat(auth): 로그인 서비스 및 UserDetails 구현 초기 설정 #4

--- src/main/java/com/sparta/twitNation/config/auth/LoginService.java
+++ src/main/java/com/sparta/twitNation/config/auth/LoginService.java
@@ -1,2 +1,25 @@
-package com.sparta.twitNation.config.auth;public class LoginService {
+package com.sparta.twitNation.config.auth;
+
+import com.sparta.twitNation.domain.user.User;
+import com.sparta.twitNation.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class LoginService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User userPS = userRepository.findByUsername(username).orElseThrow(
+                () -> new InternalAuthenticationServiceException("인증 실패")
+        );
+        return new LoginUser(userPS);
+    }
 }
--- src/main/java/com/sparta/twitNation/config/auth/LoginUser.java
+++ src/main/java/com/sparta/twitNation/config/auth/LoginUser.java
@@ -1,2 +1,52 @@
-package com.sparta.twitNation.config.auth;public class LoginUser {
+package com.sparta.twitNation.config.auth;
+
+import com.sparta.twitNation.domain.user.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+@Getter
+public class LoginUser implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        //role 관련 의논 후 설정
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
