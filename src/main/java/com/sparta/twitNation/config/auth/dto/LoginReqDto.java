af268afcae1802a854e212f1a8f6e40b233f8d64
ㅇi쀼z1 <yaewon0411@naver.com>
2024-10-20 23:59:09 +0900

    feat(auth-dto): 테스트를 위해 LoginReqDto에 빌더 및 allArgs 추가 #4

--- src/main/java/com/sparta/twitNation/config/auth/dto/LoginReqDto.java
+++ src/main/java/com/sparta/twitNation/config/auth/dto/LoginReqDto.java
@@ -1,10 +1,14 @@
 package com.sparta.twitNation.config.auth.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Getter
+@Builder
+@AllArgsConstructor
 public class LoginReqDto {
 
     private String username;
