418de98a5d3b6cc99ef3a66c1090b6edf272ef0e
ㅇi쀼z1 <yaewon0411@naver.com>
2024-10-20 22:57:46 +0900

    feat(jwt): jwt 검증 예외 처리 로직 개선 및 CustomJwtException 적용 #4 - jwt 검증 과정에서 발생하는 에러를 CustomJwtException으로 처리하도록 지정

--- src/main/java/com/sparta/twitNation/config/jwt/JwtProcess.java
+++ src/main/java/com/sparta/twitNation/config/jwt/JwtProcess.java
@@ -9,8 +9,10 @@ import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.sparta.twitNation.config.auth.LoginUser;
 import com.sparta.twitNation.domain.user.User;
+import com.sparta.twitNation.ex.CustomJwtException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -38,18 +40,17 @@ public class JwtProcess {
             User user = User.builder().id(id).build();
             return new LoginUser(user);
         }catch (TokenExpiredException e){
-            log.error("토큰이 만료되어 더 이상 유효하지 않습니다", e);
-            throw e;
+            log.error("토큰 만료: {}", e.getMessage(), e);
+            throw new CustomJwtException(HttpStatus.UNAUTHORIZED.value(), "만료된 토큰입니다");
         } catch(SignatureVerificationException e){
-            log.error("유효하지 않은 토큰 서명입니다", e);
-            throw e;
+            log.error("JWT 서명 오류: {}", e.getMessage(), e);
+            throw new CustomJwtException(HttpStatus.UNAUTHORIZED.value(), "유효하지 않은 토큰 서명 입니다");
         } catch (JWTDecodeException e) {
-            log.error("유효하지 않은 JWT 형식입니다", e);
-            throw e;
-        }
-        catch(JWTVerificationException e){
-            log.error("JWT 검증 중 오류가 발생했습니다", e);
-            throw e;
+            log.error("JWT 형식 오류: {}", e.getMessage(), e);
+            throw new CustomJwtException(HttpStatus.BAD_REQUEST.value(), "유효하지 않은 JWT 형식입니다");
+        } catch(JWTVerificationException e){
+            log.error("JWT 검증 실패: 잘못된 서명 또는 기타 오류 {}", e.getMessage(), e);
+            throw new CustomJwtException(HttpStatus.BAD_REQUEST.value(), "잘못된 JWT 토큰입니다");
         }
     }
 }
---
+++ src/main/java/com/sparta/twitNation/ex/CustomJwtException.java
@@ -0,0 +1,13 @@
+package com.sparta.twitNation.ex;
+
+public class CustomJwtException extends RuntimeException{
+
+    int status;
+    String msg;
+
+    public CustomJwtException(int status, String msg) {
+        super(msg);
+        this.status = status;
+        this.msg = msg;
+    }
+}
