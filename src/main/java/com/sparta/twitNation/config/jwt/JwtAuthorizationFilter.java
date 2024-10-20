fde2aa2aedfd0f0216656df1345e523b069a3594
ㅇi쀼z1 <yaewon0411@naver.com>
2024-10-21 00:02:05 +0900

    refactor(jwt): JwtAuthorizationFilter의 헤더 검증 오류 exceptionFilter에서 처리하도록 수정 #4

--- src/main/java/com/sparta/twitNation/config/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/sparta/twitNation/config/jwt/JwtAuthorizationFilter.java
@@ -1,6 +1,7 @@
 package com.sparta.twitNation.config.jwt;
 
 import com.sparta.twitNation.config.auth.LoginUser;
+import com.sparta.twitNation.ex.CustomJwtException;
 import com.sparta.twitNation.util.CustomUtil;
 import com.sparta.twitNation.util.api.ApiResult;
 import jakarta.servlet.FilterChain;
@@ -14,7 +15,6 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import java.io.IOException;
@@ -30,20 +30,24 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-        if(!isAuthorizationHeaderValid(request)){
-            sendErrorMessage(response, "유효하지 않거나 Authorization 헤더가 누락되었습니다");
-            return;
+        try {
+            if (!isAuthorizationHeaderValid(request)) {
+                throw new CustomJwtException(HttpStatus.UNAUTHORIZED.value(), "유효하지 않거나 Authorization 헤더가 누락되었습니다");
+            }
+            log.debug("디버그: 토큰 존재");
+            String token = request.getHeader(JwtVo.HEADER).replace(JwtVo.TOKEN_PREFIX, ""); //Bearer 제거
+
+            LoginUser loginUser = jwtProcess.verify(token);
+            log.debug("디버그: 토큰 검증 완료");
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(loginUser, null, loginUser.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            chain.doFilter(request, response); //다시 체인
+        }catch(CustomJwtException e){
+            log.warn("JWT 검증 실패: {}", e.getMessage());
+            throw e;
         }
-        log.debug("디버그: 토큰 존재");
-        String token = request.getHeader(JwtVo.HEADER).replace(JwtVo.TOKEN_PREFIX, ""); //Bearer 제거
-
-        LoginUser loginUser = jwtProcess.verify(token);
-        log.debug("디버그: 토큰 검증 완료");
-
-        Authentication authentication = new UsernamePasswordAuthenticationToken(loginUser, null, loginUser.getAuthorities());
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        chain.doFilter(request, response); //다시 체인
     }
 
     private boolean isAuthorizationHeaderValid(HttpServletRequest request){
@@ -51,14 +55,4 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
         return header != null && header.startsWith(JwtVo.TOKEN_PREFIX);
     }
 
-    private void sendErrorMessage(HttpServletResponse response, String message) throws IOException{
-        log.warn("Authorization 헤더 오류: {}", message);
-
-        ApiResult<Object> apiResult = ApiResult.error(HttpStatus.UNAUTHORIZED.value(), message);
-        String responseBody = CustomUtil.convertToJson(apiResult);
-
-        response.setContentType("application/json; charset=utf-8");
-        response.setStatus(HttpStatus.UNAUTHORIZED.value());
-        response.getWriter().println(responseBody);
-    }
 }
