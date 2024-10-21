27623d55d7b4952a24b3ac73feaf240c91ea1ce6
dbrjsdn2051 <dbrjdn2051@naver.com>
2024-10-21 17:19:18 +0900

    #13 feat(UserCreateReqDto, UserCreateRespDto) : record dto 적용

--- src/main/java/com/sparta/twitNation/dto/user/req/UserCreateReqDto.java
+++ src/main/java/com/sparta/twitNation/dto/user/req/UserCreateReqDto.java
@@ -1,4 +1,7 @@
 package com.sparta.twitNation.dto.user.req;
 
-public class UserCreateReqDto {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserCreateReqDto (@NotBlank @Email String email, String password, String nickname, String bio, String profileImg) {
 }
