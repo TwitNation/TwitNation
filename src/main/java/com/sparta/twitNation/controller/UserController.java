089a069feb19e12bf594d033479384ab696395a4
dbrjsdn2051 <dbrjdn2051@naver.com>
2024-10-21 17:21:10 +0900

    #13 feat(service, controller) : 기본적인 controller, service 구현

--- src/main/java/com/sparta/twitNation/controller/UserController.java
+++ src/main/java/com/sparta/twitNation/controller/UserController.java
@@ -1,4 +1,26 @@
 package com.sparta.twitNation.controller;
 
+import com.sparta.twitNation.dto.user.req.UserCreateReqDto;
+import com.sparta.twitNation.service.UserService;
+import com.sparta.twitNation.util.api.ApiResult;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/auth/join")
+    public ResponseEntity<ApiResult<Long>> joinUser(@RequestBody @Valid UserCreateReqDto dto) {
+        Long id = userService.addUser(dto);
+        ApiResult<Long> success = ApiResult.success(id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(success);
+    }
 }
--- src/main/java/com/sparta/twitNation/service/UserService.java
+++ src/main/java/com/sparta/twitNation/service/UserService.java
@@ -1,7 +1,12 @@
 package com.sparta.twitNation.service;
 
+import com.sparta.twitNation.domain.user.User;
 import com.sparta.twitNation.domain.user.UserRepository;
+import com.sparta.twitNation.dto.user.req.UserCreateReqDto;
+import com.sparta.twitNation.dto.user.resp.UserCreateRespDto;
+import com.sparta.twitNation.util.api.ApiResult;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,6 +15,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public Long addUser(UserCreateReqDto dto) {
+        String encodedPassword = passwordEncoder.encode(dto.password());
+        User user = new User(dto, encodedPassword);
+        return userRepository.save(user).getId();
+    }
 
 
 }
