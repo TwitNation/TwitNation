3630c824506ae748256bcfec7cb3446489d56053
ㅇi쀼z1 <yaewon0411@naver.com>
2024-10-21 21:44:13 +0900

    feat(post-controller): 게시글 수정 기능 추가 #18

--- src/main/java/com/sparta/twitNation/controller/PostController.java
+++ src/main/java/com/sparta/twitNation/controller/PostController.java
@@ -3,7 +3,9 @@ package com.sparta.twitNation.controller;
 
 import com.sparta.twitNation.config.auth.LoginUser;
 import com.sparta.twitNation.dto.post.req.PostCreateReqDto;
+import com.sparta.twitNation.dto.post.req.PostModifyReqDto;
 import com.sparta.twitNation.dto.post.resp.PostCreateRespDto;
+import com.sparta.twitNation.dto.post.resp.PostModifyRespDto;
 import com.sparta.twitNation.service.PostService;
 import com.sparta.twitNation.util.api.ApiResult;
 import jakarta.validation.Valid;
@@ -24,6 +26,13 @@ public class PostController {
     @PostMapping
     public ResponseEntity<ApiResult<PostCreateRespDto>> createPost(@RequestBody @Valid PostCreateReqDto postCreateReqDto, @AuthenticationPrincipal LoginUser loginUser){
         return new ResponseEntity<>(ApiResult.success(postService.createPost(postCreateReqDto, loginUser)), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{postId}")
+    public ResponseEntity<ApiResult<PostModifyRespDto>> modifyPost(@PathVariable(value = "postId") Long postId,
+                                                                   @RequestBody @Valid PostModifyReqDto postModifyReqDto,
+                                                                   @AuthenticationPrincipal LoginUser loginUser){
+        return new ResponseEntity<>(ApiResult.success(postService.modifyPost(postModifyReqDto, postId, loginUser)), HttpStatus.CREATED);
     }
 
 }
