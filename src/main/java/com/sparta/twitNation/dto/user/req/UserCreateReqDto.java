24e439a0ae39461f1c6665380d2ffef50c83be6d
dbrjsdn2051 <dbrjdn2051@naver.com>
2024-10-21 21:25:11 +0900

    #13 feat(UserCreateReqDto) : Valid 옵션 추가

--- src/main/java/com/sparta/twitNation/dto/user/req/UserCreateReqDto.java
+++ src/main/java/com/sparta/twitNation/dto/user/req/UserCreateReqDto.java
@@ -2,6 +2,22 @@ package com.sparta.twitNation.dto.user.req;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public record UserCreateReqDto (@NotBlank @Email String email, String password, String nickname, String bio, String profileImg) {
+public record UserCreateReqDto(
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        @Size(max = 60)
+        String password,
+
+        @NotBlank(message = "Nickname is required")
+        String nickname,
+
+        @NotBlank
+        @Size(max = 512)
+        String bio,
+        String profileImg
+) {
 }
