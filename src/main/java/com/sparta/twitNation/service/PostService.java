9a12a7db27174d69ef4864de4b59e2abfbc3a3d9
yuseonjun <seonjun0906@gmail.com>
2024-10-21 20:02:42 +0900

    #15 feat(PostService) : 조회할때 수정 일자 최신순으로 정렬 추가

--- src/main/java/com/sparta/twitNation/service/PostService.java
+++ src/main/java/com/sparta/twitNation/service/PostService.java
@@ -13,6 +13,7 @@ import com.sparta.twitNation.ex.CustomApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,7 +32,8 @@ public class PostService {
     public UserPostsRespDto readPostsBy(final Long userId, final int page, final int limit) {
         final User user = userRepository.findById(userId).orElseThrow(
                 () -> new CustomApiException("존재하지 않는 유저입니다", HttpStatus.NOT_FOUND.value()));
-        final Page<Post> posts = postRepository.findByUser(user, PageRequest.of(page, limit));
+        final Page<Post> posts = postRepository.findByUser(user,
+                PageRequest.of(page, limit, Sort.by(Sort.Direction.DESC, "lastModifiedAt")));
 
         final Page<PostReadPageRespDto> response = posts.map(
                 post -> {
