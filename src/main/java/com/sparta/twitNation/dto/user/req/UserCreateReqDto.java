903119ce6951d53684e259b0c23f1bcd2a2c3d58
dbrjsdn2051 <dbrjdn2051@naver.com>
2024-10-22 10:39:24 +0900

    refactor(userCreateReqDto) : 패스워 드 정규식 수정

--- src/main/java/com/sparta/twitNation/dto/user/req/UserCreateReqDto.java
+++ src/main/java/com/sparta/twitNation/dto/user/req/UserCreateReqDto.java
@@ -9,9 +9,11 @@ public record UserCreateReqDto(
         @NotBlank(message = "이메일을 입력헤주세요.")
         @Email(message = "정확한 이메일 형식으로 입력해주세요. ")
         String email,
+
+
         @NotBlank
         @Size(min = 8, max = 20, message = "패스워드 글자 수는 60자까지만 입력해주세요.")
-        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", // test : password1!
                 message = "비밀번호는 대소문자 포함 영문 + 숫자 + 특수문자를 최소 1글자씩 최소 8글자 이상이어야 합니다.")
         String password,
 
