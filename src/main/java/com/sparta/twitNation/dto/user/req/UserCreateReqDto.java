bc2480d0dc457de5d611ef05f8403e3fab1a30bc
dbrjsdn2051 <dbrjdn2051@naver.com>
2024-10-22 11:12:34 +0900

    #13 refactor(UserCreateReqDto) : 정규식 주석 추가

--- src/main/java/com/sparta/twitNation/dto/user/req/UserCreateReqDto.java
+++ src/main/java/com/sparta/twitNation/dto/user/req/UserCreateReqDto.java
@@ -13,8 +13,8 @@ public record UserCreateReqDto(
 
         @NotBlank
         @Size(min = 8, max = 20, message = "패스워드 글자 수는 60자까지만 입력해주세요.")
-//        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", // test : password1!
-//                message = "비밀번호는 대소문자 포함 영문 + 숫자 + 특수문자를 최소 1글자씩 최소 8글자 이상이어야 합니다.")
+        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", // test : password1!
+                message = "비밀번호는 대소문자 포함 영문 + 숫자 + 특수문자를 최소 1글자씩 최소 8글자 이상이어야 합니다.")
         String password,
 
         @NotBlank(message = "닉네임을 입력해주세요.")
