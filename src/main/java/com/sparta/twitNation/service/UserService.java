94f025f289ddce7ae6d6a5eb41ad8b2d417a91b6
dbrjsdn2051 <dbrjdn2051@naver.com>
2024-10-22 15:24:24 +0900

    #13 refactor(ErrorCode) : username 필드 제거

--- src/main/java/com/sparta/twitNation/service/UserService.java
+++ src/main/java/com/sparta/twitNation/service/UserService.java
@@ -4,6 +4,8 @@ import com.sparta.twitNation.domain.user.User;
 import com.sparta.twitNation.domain.user.UserRepository;
 import com.sparta.twitNation.dto.user.req.UserCreateReqDto;
 import com.sparta.twitNation.dto.user.resp.UserCreateRespDto;
+import com.sparta.twitNation.ex.CustomApiException;
+import com.sparta.twitNation.ex.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -21,6 +23,9 @@ public class UserService {
         String encodedPassword = passwordEncoder.encode(dto.password());
         User user = new User(dto.passwordEncoded(encodedPassword));
         Optional<User> userOP = userRepository.findByEmail(user.getEmail());
+        if (userOP.isPresent()) {
+            throw new CustomApiException(ErrorCode.ALREADY_USER_EXIST);
+        }
 
         return new UserCreateRespDto(userRepository.save(user).getId());
     }
