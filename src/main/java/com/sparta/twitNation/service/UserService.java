01458b54e6788d0a6638a589efd9f1b85bff7230
dbrjsdn2051 <dbrjdn2051@naver.com>
2024-10-22 18:15:25 +0900

    #22 fix(pull) : 충돌 해결

--- src/main/java/com/sparta/twitNation/service/UserService.java
+++ src/main/java/com/sparta/twitNation/service/UserService.java
@@ -16,8 +16,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 public class UserService {
