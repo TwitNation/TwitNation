e3d27cc85a1dd38e826411dcc10aba8c16515fc6
dbrjsdn2051 <dbrjdn2051@naver.com>
2024-10-22 21:23:45 +0900

    [#40] feat(UserController) : controller 기능 재 업로드

--- src/main/java/com/sparta/twitNation/controller/UserController.java
+++ src/main/java/com/sparta/twitNation/controller/UserController.java
@@ -1,16 +1,20 @@
 package com.sparta.twitNation.controller;
 
+import com.sparta.twitNation.config.auth.LoginUser;
 import com.sparta.twitNation.dto.user.req.UserCreateReqDto;
+import com.sparta.twitNation.dto.user.req.UserUpdateReqDto;
 import com.sparta.twitNation.dto.user.resp.UserCreateRespDto;
+import com.sparta.twitNation.dto.user.resp.UserEditPageRespDto;
+import com.sparta.twitNation.dto.user.resp.UserInfoRespDto;
+import com.sparta.twitNation.dto.user.resp.UserUpdateRespDto;
 import com.sparta.twitNation.service.UserService;
 import com.sparta.twitNation.util.api.ApiResult;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -23,7 +27,7 @@ public class UserController {
     public ResponseEntity<ApiResult<UserCreateRespDto>> joinUser(
             @RequestPart("user") @Valid UserCreateReqDto dto,
             @RequestPart(value = "profileImg", required = false) MultipartFile profileImg
-            ) {
+    ) {
 
         String profileImgUrl = null;
         // S3 생성 로직 ...
@@ -31,5 +35,26 @@ public class UserController {
         UserCreateRespDto RespDto = userService.register(dto);
         ApiResult<UserCreateRespDto> success = ApiResult.success(RespDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(success);
+    }
+
+    @GetMapping("/api/user/profile")
+    public ResponseEntity<ApiResult<UserEditPageRespDto>> editPage(@AuthenticationPrincipal LoginUser loginUser) {
+        UserEditPageRespDto dto = userService.editList(loginUser.getUser().getId());
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResult.success(dto));
+    }
+
+    @PatchMapping("/api/user/profile")
+    public ResponseEntity<ApiResult<UserUpdateRespDto>> updateUserInfo(
+            @AuthenticationPrincipal LoginUser loginUser,
+            @RequestBody @Valid UserUpdateReqDto dto
+    ) {
+        UserUpdateRespDto respDto = userService.updateUser(loginUser.getUser().getId(), dto);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResult.success(respDto));
+    }
+
+    @GetMapping("/api/users/profile/{userId}")
+    public ResponseEntity<ApiResult<UserInfoRespDto>> myPage(@PathVariable Long userId) {
+        UserInfoRespDto respDto = userService.userInfo(userId);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResult.success(respDto));
     }
 }
