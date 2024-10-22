3727c3cb89a61135434d4d693ca095958bb34fb3
dbrjsdn2051 <dbrjdn2051@naver.com>
2024-10-22 17:06:57 +0900

    #22 fix(merge): 충돌 해결

--- src/main/java/com/sparta/twitNation/domain/user/User.java
+++ src/main/java/com/sparta/twitNation/domain/user/User.java
@@ -35,6 +35,9 @@ public class User extends BaseEntity {
     public User(Long id, String email, String nickname, String bio, String profileImg, String password) {
         this.id = id;
         this.email = email;
+        this.nickname = nickname;
+        this.bio = bio;
+        this.profileImg = profileImg;
         this.password = password;
     }
 
