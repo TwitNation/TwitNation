f65e7fd427820e5da40977dfbf60e18607fb911d
dbrjsdn2051 <dbrjdn2051@naver.com>
2024-10-22 17:04:55 +0900

    #22 fix(merge): 충돌 해결

--- src/main/java/com/sparta/twitNation/ex/ErrorCode.java
+++ src/main/java/com/sparta/twitNation/ex/ErrorCode.java
@@ -1,6 +1,6 @@
 package com.sparta.twitNation.ex;
 
-import com.sparta.twitNation.service.UserService;
+
 import lombok.Getter;
 
 @Getter
@@ -10,7 +10,7 @@ public enum ErrorCode {
     INTERNAL_SERVER_ERROR(500, "서버 내부 오류"),
     POST_FORBIDDEN(403, "해당 게시글을 수정할 권한이 없습니다"),
     POST_NOT_FOUND(404, "존재하지 않는 게시글입니다"),
-    COMMENT_FORBIDEN(403,"해당 댓글을 수정할 권한이 없습니다."),
+    COMMENT_FORBIDEN(403, "해당 댓글을 수정할 권한이 없습니다."),
     COMMENT_NOT_FOUND(404, "존재하지 않는 댓글입니다."),
     ALREADY_USER_EXIST(400, "존재하는 사용자입니다.");
 
