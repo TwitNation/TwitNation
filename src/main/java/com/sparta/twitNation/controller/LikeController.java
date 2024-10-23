b0441798ee06de2f2f0934eaed0b1d246b41116f
dbrjsdn2051 <dbrjdn2051@naver.com>
2024-10-23 13:32:41 +0900

    [#51] feat(LikeController) : controller 구현

--- src/main/java/com/sparta/twitNation/controller/LikeController.java
+++ src/main/java/com/sparta/twitNation/controller/LikeController.java
@@ -2,15 +2,17 @@ package com.sparta.twitNation.controller;
 
 import com.sparta.twitNation.config.auth.LoginUser;
 import com.sparta.twitNation.dto.like.resp.LikeCreateRespDto;
+import com.sparta.twitNation.dto.like.resp.LikeReadPageRespDto;
 import com.sparta.twitNation.service.LikeService;
 import com.sparta.twitNation.util.api.ApiResult;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,6 +27,16 @@ public class LikeController {
     ) {
         LikeCreateRespDto respDto = likeService.toggleLike(loginUser.getUser(), postId);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResult.success(respDto));
+    }
+
+    @GetMapping("/api/users/likes")
+    public ResponseEntity<ApiResult<Page<LikeReadPageRespDto>>> likePage(
+            @AuthenticationPrincipal LoginUser loginUser,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        Page<LikeReadPageRespDto> respDtos = likeService.likePosts(loginUser.getUser().getId(), PageRequest.of(page, size));
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResult.success(respDtos));
     }
 
 }
