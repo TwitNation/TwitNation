7e6a9e70ca03c4b1b70762bba2848038fd4c8e69
ㅇi쀼z1 <yaewon0411@naver.com>
2024-10-23 19:47:09 +0900

    feat(swagger): 게시글 스웨거 설정 #59

--- src/main/java/com/sparta/twitNation/config/SwaggerConfig.java
+++ src/main/java/com/sparta/twitNation/config/SwaggerConfig.java
@@ -1,11 +1,18 @@
 package com.sparta.twitNation.config;
 
+import com.sparta.twitNation.util.api.ApiError;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
 
 @Configuration
 public class SwaggerConfig {
--- src/main/java/com/sparta/twitNation/controller/PostControllerDocs.java
+++ src/main/java/com/sparta/twitNation/controller/PostControllerDocs.java
@@ -1,2 +1,28 @@
-package com.sparta.twitNation.controller;public interface PostControllerDocs {
+package com.sparta.twitNation.controller;
+
+import com.sparta.twitNation.config.auth.LoginUser;
+import com.sparta.twitNation.dto.post.req.PostCreateReqDto;
+import com.sparta.twitNation.dto.post.resp.PostCreateRespDto;
+import com.sparta.twitNation.dto.user.resp.UserUpdateRespDto;
+import com.sparta.twitNation.util.api.ApiResult;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.RequestBody;
+@Tag(name = "게시글 API", description = "게시글 관련 기능 API ")
+public interface PostControllerDocs {
+
+    @Operation(summary = "게시글 생성 ", description = "게시글 생성 기능: jwt 토큰 사용")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "게시글 생성 성공",content = @Content(schema = @Schema(implementation = PostCreateRespDto.class), mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "Authorization 헤더가 누락되었습니다",content = @Content(schema = @Schema(implementation = PostCreateRespDto.class), mediaType = "application/json")),
+
+    })
+    ResponseEntity<ApiResult<PostCreateRespDto>> createPost(@RequestBody @Valid PostCreateReqDto postCreateReqDto, @AuthenticationPrincipal LoginUser loginUser);
 }
