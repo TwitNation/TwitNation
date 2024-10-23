cc655c3b8904b244ace40d78d4324df9c7bc18a9
dbrjsdn2051 <dbrjdn2051@naver.com>
2024-10-23 13:18:56 +0900

    [#51] feat(LikeService) : service 구현

--- src/main/java/com/sparta/twitNation/service/LikeService.java
+++ src/main/java/com/sparta/twitNation/service/LikeService.java
@@ -1,14 +1,20 @@
 package com.sparta.twitNation.service;
 
+import com.sparta.twitNation.domain.comment.CommentRepository;
 import com.sparta.twitNation.domain.like.Like;
+import com.sparta.twitNation.domain.like.LikeCustomRepository;
 import com.sparta.twitNation.domain.like.LikeRepository;
 import com.sparta.twitNation.domain.post.Post;
 import com.sparta.twitNation.domain.post.PostRepository;
+import com.sparta.twitNation.domain.retweet.RetweetRepository;
 import com.sparta.twitNation.domain.user.User;
 import com.sparta.twitNation.dto.like.resp.LikeCreateRespDto;
+import com.sparta.twitNation.dto.like.resp.LikeReadPageRespDto;
 import com.sparta.twitNation.ex.CustomApiException;
 import com.sparta.twitNation.ex.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -19,6 +25,7 @@ public class LikeService {
 
     private final PostRepository postRepository;
     private final LikeRepository likeRepository;
+    private final LikeCustomRepository likeCustomRepository;
 
     public LikeCreateRespDto toggleLike(User user, Long postId) {
         Optional<Like> likeOP = likeRepository.findByPostId(postId);
@@ -36,4 +43,9 @@ public class LikeService {
 
         return new LikeCreateRespDto(postId, true);
     }
+
+    public Page<LikeReadPageRespDto> likePosts(Long userId, Pageable pageable) {
+        return likeCustomRepository.searchLikes(userId, pageable);
+    }
+
 }
