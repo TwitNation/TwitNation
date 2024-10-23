500d5e0ea19bc806001c5ee05dad45e3dcb17b4d
siyeon02 <chrysapark@cau.ac.kr>
2024-10-23 10:17:26 +0900

    feat(bookmarks):북마크 추가 ë기능 완료 #27

--- src/main/java/com/sparta/twitNation/config/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/sparta/twitNation/config/jwt/JwtAuthorizationFilter.java
@@ -4,12 +4,15 @@ import com.sparta.twitNation.config.auth.LoginUser;
 import com.sparta.twitNation.ex.CustomApiException;
 import com.sparta.twitNation.ex.CustomJwtException;
 import com.sparta.twitNation.ex.ErrorCode;
+import com.sparta.twitNation.util.CustomUtil;
+import com.sparta.twitNation.util.api.ApiResult;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -31,37 +34,31 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         try {
-//            if(request.getServletPath().startsWith("/api/bookmarks")){
-//                chain.doFilter(request,response);
-//                return;
-//            }
-            if (request.getRequestURI().equals("/auth/join")) {
 
-                if (request.getRequestURI().contains("auth")) {
-                    chain.doFilter(request, response);
-                    return;
-                }
-
-                if (!isAuthorizationHeaderValid(request)) {
-                    throw new CustomApiException(ErrorCode.TOKEN_MISSING);
-                }
-
-                log.debug("디버그: 토큰 존재");
-                String token = request.getHeader(JwtVo.HEADER).replace(JwtVo.TOKEN_PREFIX, ""); //Bearer 제거
-
-                LoginUser loginUser = jwtProcess.verify(token);
-                log.debug("디버그: 토큰 검증 완료");
-
-                Authentication authentication = new UsernamePasswordAuthenticationToken(loginUser, null, loginUser.getAuthorities());
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-                log.debug("시큐리티 컨텍스트: {}", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-
-                chain.doFilter(request, response); //다시 체인
+            if(request.getRequestURI().contains("auth")) {
+                chain.doFilter(request, response);
+                return;
             }
-        } catch (CustomJwtException e) {
+
+            if (!isAuthorizationHeaderValid(request)) {
+                throw new CustomApiException(ErrorCode.TOKEN_MISSING);
+            }
+
+            log.debug("디버그: 토큰 존재");
+            String token = request.getHeader(JwtVo.HEADER).replace(JwtVo.TOKEN_PREFIX, ""); //Bearer 제거
+
+            LoginUser loginUser = jwtProcess.verify(token);
+            log.debug("디버그: 토큰 검증 완료");
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(loginUser, null, loginUser.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            log.debug("시큐리티 컨텍스트: {}", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+
+            chain.doFilter(request, response); //다시 체인
+        } catch (CustomJwtException  e) {
             log.warn("JWT 검증 실패: {}", e.getMessage());
             throw e;
-        } catch (CustomApiException e){
+        } catch(CustomApiException e){
             log.warn("헤더 누락: {}", e.getMessage());
             throw e;
         }
