f6975931df02b4f169b438b07dc060155161bcff
dbrjsdn2051 <dbrjdn2051@naver.com>
2024-10-23 09:46:28 +0900

    [#42] refactor(UserCreateReqDto, UserUpdateReqDto) : @Size 메시지 수정

--- src/main/java/com/sparta/twitNation/dto/user/req/UserUpdateReqDto.java
+++ src/main/java/com/sparta/twitNation/dto/user/req/UserUpdateReqDto.java
@@ -11,7 +11,7 @@ public record UserUpdateReqDto(
         String email,
 
         @NotBlank
-        @Size(min = 8, max = 20, message = "패스워드 글자 수는 60자까지만 입력해주세요.")
+        @Size(min = 8, max = 20, message = "패스워드 글자 수는 20자까지만 입력해주세요.")
         @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", // test : password1!
                 message = "비밀번호는 대소문자 포함 영문 + 숫자 + 특수문자를 최소 1글자씩 최소 8글자 이상이어야 합니다.")
         String password,
