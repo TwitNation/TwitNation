f8203965db8342442a071c1b3e2f65a6f62cfb0b
caose <rlagkrwls258@gmail.com>
2024-10-23 16:39:07 +0900

    docs(comments) : swagger 문서 작성

--- src/main/java/com/sparta/twitNation/controller/CommentController.java
+++ src/main/java/com/sparta/twitNation/controller/CommentController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 @Validated
-public class CommentController {
+public class CommentController implements CommentControllerDocs {
 
     private final CommentService commentService;
 
