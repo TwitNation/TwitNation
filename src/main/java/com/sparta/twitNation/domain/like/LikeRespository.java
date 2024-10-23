934f30493a1454102e0fe030cc082a2ca00f768a
siyeon02 <chrysapark@cau.ac.kr>
2024-10-23 15:36:33 +0900

    feat(bookmarks):likeCount #27

--- src/main/java/com/sparta/twitNation/domain/like/LikeRespository.java
+++
@@ -1,6 +0,0 @@
-package com.sparta.twitNation.domain.like;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LikeRespository extends JpaRepository<Like, Long> {
-}
