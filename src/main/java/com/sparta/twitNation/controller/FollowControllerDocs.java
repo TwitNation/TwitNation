c06d3d84f59d7a75bf7b2177820b10e791a388fa
siyeon02 <chrysapark@cau.ac.kr>
2024-10-23 22:19:56 +0900

    FollowControllerDocs 형식 맞추기

--- src/main/java/com/sparta/twitNation/controller/FollowControllerDocs.java
+++ src/main/java/com/sparta/twitNation/controller/FollowControllerDocs.java
@@ -6,6 +6,8 @@ import com.sparta.twitNation.dto.bookmark.resp.BookmarkCreateRespDto;
 import com.sparta.twitNation.dto.bookmark.resp.BookmarkViewRespDto;
 import com.sparta.twitNation.dto.follow.resp.FollowCreateRespDto;
 import com.sparta.twitNation.dto.follow.resp.FollowerViewRespDto;
+import com.sparta.twitNation.dto.follow.resp.FollowingReadPageRespDto;
+import com.sparta.twitNation.dto.post.resp.PostCreateRespDto;
 import com.sparta.twitNation.util.api.ApiResult;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -21,25 +23,49 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "팔로우 API", description = "팔로우 관련 API ")
 public interface FollowControllerDocs {
+
+    @Operation(summary = "팔로잉 조회", description = "팔로잉한 유저 정보 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "팔로잉 조회 성공", content = @Content(schema = @Schema(implementation = FollowingReadPageRespDto.class), mediaType = "application/json")),
+            @ApiResponse(responseCode = "403", description = "유효성 검사 실패", content = @Content(schema = @Schema(implementation = FollowingReadPageRespDto.class), mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "인증이 필요합니다", content = @Content(schema = @Schema(implementation = FollowingReadPageRespDto.class), mediaType = "application/json")),
+            @ApiResponse(responseCode = "403", description = "권한이 필요합니다",content = @Content(schema = @Schema(implementation = FollowingReadPageRespDto.class), mediaType = "application/json")),
+            @ApiResponse(responseCode = "500", description = "서버 내부에 오류가 발생헀습니다. 잠시 후에 시도해주세요",content = @Content(schema = @Schema(implementation = FollowingReadPageRespDto.class), mediaType = "application/json"))
+
+    })
+    ResponseEntity<ApiResult<FollowingReadPageRespDto>> followings(
+            @PathVariable Long userId,
+            @RequestParam(defaultValue = "0") @Parameter(description = "페이지 번호") int page ,
+            @RequestParam(defaultValue = "10") @Parameter(description = "페이지에 나갈 크기") int limit
+    );
+
+
     @Operation(summary = "팔로우 상태 변경 ", description = "유저를 팔로우에 추가/삭제")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "팔로우 추가/삭제 완료", content = @Content(schema =
-            @Schema(implementation = FollowCreateRespDto.class), mediaType = "application/json")),
-            @ApiResponse(responseCode = "401", description = "Authorization 헤더 재확인 바람", content = @Content(schema =
-            @Schema(implementation = FollowCreateRespDto.class), mediaType = "application/json"))
+            @ApiResponse(responseCode = "200", description = "팔로우 추가/삭제 성공", content = @Content(schema = @Schema(implementation = FollowCreateRespDto.class), mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "유효성 검사 실패",content = @Content(schema = @Schema(implementation = FollowCreateRespDto.class), mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "인증이 필요합니다", content = @Content(schema = @Schema(implementation = FollowCreateRespDto.class), mediaType = "application/json")),
+            @ApiResponse(responseCode = "403", description = "권한이 필요합니다",content = @Content(schema = @Schema(implementation = FollowCreateRespDto.class), mediaType = "application/json")),
+            @ApiResponse(responseCode = "500", description = "서버 내부에 오류가 발생헀습니다. 잠시 후에 시도해주세요",content = @Content(schema = @Schema(implementation = FollowCreateRespDto.class), mediaType = "application/json"))
     })
     ResponseEntity<ApiResult<FollowCreateRespDto>> changeFollowState(@PathVariable(name = "postId") Long postId, @AuthenticationPrincipal LoginUser loginUser);
 
     @Operation(summary = "팔로우 조회", description = "팔로우한 유저 정보 조회")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "팔로워 조회 완료", content = @Content(schema =
-            @Schema(implementation = FollowerViewRespDto.class), mediaType = "application/json")),
-            @ApiResponse(responseCode = "403", description = "자기 자신은 팔로우 불가", content = @Content(schema =
-            @Schema(implementation = FollowerViewRespDto.class), mediaType = "application/json"))
+            @ApiResponse(responseCode = "200", description = "팔로워 조회 성공", content = @Content(schema = @Schema(implementation = FollowerViewRespDto.class), mediaType = "application/json")),
+            @ApiResponse(responseCode = "403", description = "유효성 검사 실패", content = @Content(schema = @Schema(implementation = FollowerViewRespDto.class), mediaType = "application/json")),
+            @ApiResponse(responseCode = "401", description = "인증이 필요합니다", content = @Content(schema = @Schema(implementation = FollowerViewRespDto.class), mediaType = "application/json")),
+            @ApiResponse(responseCode = "403", description = "권한이 필요합니다",content = @Content(schema = @Schema(implementation = FollowerViewRespDto.class), mediaType = "application/json")),
+            @ApiResponse(responseCode = "500", description = "서버 내부에 오류가 발생헀습니다. 잠시 후에 시도해주세요",content = @Content(schema = @Schema(implementation = FollowerViewRespDto.class), mediaType = "application/json"))
+
     })
     ResponseEntity<ApiResult<FollowerViewRespDto>> getFollowers(
             @RequestParam(defaultValue = "0") @Parameter(description = "페이지 번호") int page ,
             @RequestParam(defaultValue = "10") @Parameter(description = "페이지에 나갈 크기") int limit,
             @AuthenticationPrincipal LoginUser loginUser
     );
+
+
+
+
 }
