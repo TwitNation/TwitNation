e31191fc05c75d44ce693718b3c3090b88e40c3d
caose <rlagkrwls258@gmail.com>
2024-10-23 23:53:42 +0900

    refactor(join) : 댓글 isDeleted 제거

--- src/main/java/com/sparta/twitNation/dto/comment/resp/CommentDeleteRespDto.java
+++ src/main/java/com/sparta/twitNation/dto/comment/resp/CommentDeleteRespDto.java
@@ -4,7 +4,5 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 public record CommentDeleteRespDto(
         @Schema(description = "삭제된 댓글의 Id를 출력합니다.", required = true)
-        Long commentId,
-        @Schema(description = "댓글이 정상적으로 삭제되었는지 상태값을 출력합니다.", required = true)
-        boolean isDeleted) {
+        Long commentId) {
 }
