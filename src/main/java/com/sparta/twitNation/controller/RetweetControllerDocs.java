2ad3a0e55e26159bdddf8fac749884a49f9bbc3b
ㅇi쀼z1 <yaewon0411@naver.com>
2024-10-24 09:40:34 +0900

    feat(swagger): 명세 태그 추가

--- src/main/java/com/sparta/twitNation/controller/RetweetControllerDocs.java
+++ src/main/java/com/sparta/twitNation/controller/RetweetControllerDocs.java
@@ -6,11 +6,13 @@ import com.sparta.twitNation.util.api.ApiResult;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@Tag(name = "리트윗 API", description = "리트윗 관련 API ")
 public interface RetweetControllerDocs {
     @Operation(summary = "리트윗 토글(리트윗하기/리트윗해제)", description = "한 번의 요청으로 리트윗 요청/해제에 대한 결과를 리턴합니다.")
     @ApiResponses({
